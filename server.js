9aa5e016429f6d409e73c48eea0496c0642af10b
kutmanbaastanov7-star <kutmanbaastanov7@gmail.com>
2026-08-28 21:02:53 +0600

    server.js

--- server.js
+++ server.js
@@ -2,46 +2,14 @@
    SPA FALLBACK
 ========================= */
 
-app.get(
-  "/*splat",
-  (req, res, next) => {
+app.use((req, res, next) => {
 
-    if (
-      req.path.startsWith("/api/")
-    ) {
-      return next();
-    }
-
-    res.sendFile(
-      path.join(
-        __dirname,
-        "index.html"
-      )
-    );
-
+  if (req.path.startsWith("/api/")) {
+    return next();
   }
-);
 
-/* =========================
-   START SERVER
-========================= */
+  res.sendFile(
+    path.join(__dirname, "index.html")
+  );
 
-server.listen(
-  PORT,
-  "0.0.0.0",
-  () => {
-
-    console.log(
-      `🚀 Apex Agro server running on port ${PORT}`
-    );
-
-    console.log(
-      `🌱 Users: ${db.users.length}`
-    );
-
-    console.log(
-      `📦 Ads: ${db.ads.length}`
-    );
-
-  }
-);
+});
